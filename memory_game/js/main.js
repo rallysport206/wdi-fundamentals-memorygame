70009fd2cc78a6d7f1575c28a017719966e51a76
rallysport206 <dasandman02@hotmail.com>
2017-11-13 13:04:29 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
