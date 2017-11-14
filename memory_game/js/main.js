636ebda35183ba95358e95c3e4a989ea3cb5ea05
rallysport206 <dasandman02@hotmail.com>
2017-11-13 20:30:27 -0800

    js updated unit 8

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,24 @@
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+alert('Hello, friends.');
+/* card array */
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User Flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User Flipped " + cardTwo);
+
+var checkForMatch = function (){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	}else {
+		console.log("Sorry, try again");
+	}
+}
+
+
 
