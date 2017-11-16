6fb861280041510c8ccb9f6edd12039d9b8a4d3c
rallysport206 <dasandman02@hotmail.com>
2017-11-16 14:00:21 -0800

    updated with assignment 9

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,29 @@
-alert('Hello, friends.');
 /* card array */
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+var cardId = cards[0,1,2,3];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User Flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User Flipped " + cardTwo);
-
-var checkForMatch = function (){
+/* checking for card matches*/
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
-	}else {
-		console.log("Sorry, try again");
+	}else{
+		console.log("Sorry, try again.");
 	}
-}
+};
 
+var flipCard = function(cardID) {
+console.log ("User flipped " + cards[cardId]);
+	if (cardsInPlay[0] === cardsInPlay [1]) {
+			alert("You've found a match!");
+		}else{
+			alert("Sorry, try again.");
+		}
+	};
 
-
+cardsInPlay.push(cards[cardId]);
+flipCard(0);
+flipCard(2);
+checkForMatch(flipCard);
+  
