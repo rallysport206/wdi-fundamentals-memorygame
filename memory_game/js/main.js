37b7732f9bb3e70eee5612992e21b7bba139a1d2
rallysport206 <dasandman02@hotmail.com>
2017-11-17 10:29:50 -0800

    unit 10 update with card rank, suit, image.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,29 +1,44 @@
-/* card array */
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 
-var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardId = cards[0,1,2,3];
-
-/* checking for card matches*/
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-	}else{
-		console.log("Sorry, try again.");
-	}
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!") } 
+else {
+alert("Sorry, try again") }
 };
 
-var flipCard = function(cardID) {
-console.log ("User flipped " + cards[cardId]);
-	if (cardsInPlay[0] === cardsInPlay [1]) {
-			alert("You've found a match!");
-		}else{
-			alert("Sorry, try again.");
-		}
-	};
+var flipCard = function (cardId) {
+cardsInPlay.push(cards[cardId].rank);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+if (cardsInPlay.length === 2) {
+checkForMatch();
+}
+};
 
-cardsInPlay.push(cards[cardId]);
 flipCard(0);
 flipCard(2);
-checkForMatch(flipCard);
-  
