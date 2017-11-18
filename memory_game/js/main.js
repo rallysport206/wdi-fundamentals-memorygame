69bc9b55868a23960b77c656d3bf13e11f6cf849
rallysport206 <dasandman02@hotmail.com>
2017-11-17 21:16:30 -0800

    updated unit 11 game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,22 +2,22 @@ var cards = [
 {
 rank: "queen",
 suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
+cardImage: "images/queen-of-hearts.png",
 },
 {
 rank: "queen",
 suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png"
+cardImage: "images/queen-of-diamonds.png",
 },
 {
 rank: "king",
 suit: "hearts",
-cardImage: "images/king-of-hearts.png"
+cardImage: "images/king-of-hearts.png",
 },
 {
 rank: "king",
 suit: "diamonds",
-cardImage: "images/king-of-diamonds.png"
+cardImage: "images/king-of-diamonds.png",
 }
 ];
 
@@ -25,20 +25,38 @@ var cardsInPlay = [];
 var checkForMatch = function() {
 
 if (cardsInPlay[0] === cardsInPlay[1]) {
-alert("You found a match!") } 
+alert("You found a match!"); } 
 else {
-alert("Sorry, try again") }
+alert("Sorry, try again"); }
 };
 
-var flipCard = function (cardId) {
-cardsInPlay.push(cards[cardId].rank);
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-if (cardsInPlay.length === 2) {
-checkForMatch();
+var flipCard = function () {
+    var cardId = this.getAttribute('data-id');
+
+    this.setAttribute('src', cards[cardId].cardImage);
+    
+
+    console.log("User Flipped " + cards[cardId].rank);
+    
+    cardsInPlay.push(cards[cardId].rank);
+    
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    if(cardsInPlay.length === 2) {
+    checkForMatch ();
+
+    
 }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click',flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
